d9c0d90506f9a57df83bf2272539f2a623f12a66
Gilnei Gonzo <gilbegprog@gmail.com>
2022-11-14 15:36:15 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return (
     <>
       <h1>Hello World !!!</h1>
-      <h2>Gilnei</h2>
+      <NotificationButton />
     </>
   )
 }
